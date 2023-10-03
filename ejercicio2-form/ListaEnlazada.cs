80f369712981cdb240e3d09e20f6a2d4ea2687fc
jorgeanthony torrespastor <torrespastor4@hotmail.com>
2023-10-02 19:13:15 -0500

    Nuevos métodos: EliminarEnPosicion y CalcularMediaGeometrica

--- ejercicio2-form/ListaEnlazada.cs
+++ ejercicio2-form/ListaEnlazada.cs
@@ -39,6 +39,57 @@ namespace ejercicio2_form
             return suma / contador;
         }
 
+        public void EliminarEnPosicion(int posicion)
+        {
+            if (posicion <= 0 || cabeza == null)
+            {
+                Console.WriteLine("Posición no válida. No se ha eliminado ningún nodo.");
+                return;
+            }
+
+            if (posicion == 1)
+            {
+                cabeza = cabeza.Siguiente;
+                return;
+            }
+
+            Nodo actual = cabeza;
+            int contador = 1;
+
+            while (actual != null && contador < posicion - 1)
+            {
+                actual = actual.Siguiente;
+                contador++;
+            }
+
+            if (actual == null || actual.Siguiente == null)
+            {
+                Console.WriteLine("Posición no válida. No se ha eliminado ningún nodo.");
+                return;
+            }
+
+            actual.Siguiente = actual.Siguiente.Siguiente;
+        }
+
+        public double CalcularMediaGeometrica()
+        {
+            if (cabeza == null)
+                return 0;
+
+            double producto = 1;
+            int contador = 0;
+            Nodo actual = cabeza;
+
+            while (actual != null)
+            {
+                producto *= actual.Valor;
+                actual = actual.Siguiente;
+                contador++;
+            }
+
+            return Math.Pow(producto, 1.0 / contador);
+        }
+
         public List<double> ObtenerValoresMenoresAlPromedio()
         {
             double promedio = CalcularPromedio();
--- ejercicio2-form/trabajo_en_equipo.cs
+++ ejercicio2-form/trabajo_en_equipo.cs
@@ -12,56 +12,7 @@ namespace ejercicio2_form
 
         
 
-        public void EliminarEnPosicion(int posicion)
-        {
-            if (posicion <= 0 || cabeza == null)
-            {
-                Console.WriteLine("Posición no válida. No se ha eliminado ningún nodo.");
-                return;
-            }
-
-            if (posicion == 1)
-            {
-                cabeza = cabeza.Siguiente;
-                return;
-            }
-
-            Nodo actual = cabeza;
-            int contador = 1;
-
-            while (actual != null && contador < posicion - 1)
-            {
-                actual = actual.Siguiente;
-                contador++;
-            }
-
-            if (actual == null || actual.Siguiente == null)
-            {
-                Console.WriteLine("Posición no válida. No se ha eliminado ningún nodo.");
-                return;
-            }
-
-            actual.Siguiente = actual.Siguiente.Siguiente;
-        }
-
-        public double CalcularMediaGeometrica()
-        {
-            if (cabeza == null)
-                return 0;
-
-            double producto = 1;
-            int contador = 0;
-            Nodo actual = cabeza;
-
-            while (actual != null)
-            {
-                producto *= actual.Valor;
-                actual = actual.Siguiente;
-                contador++;
-            }
-
-            return Math.Pow(producto, 1.0 / contador);
-        }
+        
 
         */
     }
