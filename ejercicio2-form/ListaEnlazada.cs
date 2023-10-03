94f3697c7a13b58047cd9185bbc93288e6b7c6e5
jorgeanthony torrespastor <torrespastor4@hotmail.com>
2023-10-02 19:06:54 -0500

    Nuevos metodo: insertar y calcular promedio

--- ejercicio2-form/ListaEnlazada.cs
+++ ejercicio2-form/ListaEnlazada.cs
@@ -20,57 +20,6 @@ namespace ejercicio2_form
             cabeza = nuevoNodo;
         }
 
-        public void EliminarEnPosicion(int posicion)
-        {
-            if (posicion <= 0 || cabeza == null)
-            {
-                Console.WriteLine("Posición no válida. No se ha eliminado ningún nodo.");
-                return;
-            }
-
-            if (posicion == 1)
-            {
-                cabeza = cabeza.Siguiente;
-                return;
-            }
-
-            Nodo actual = cabeza;
-            int contador = 1;
-
-            while (actual != null && contador < posicion - 1)
-            {
-                actual = actual.Siguiente;
-                contador++;
-            }
-
-            if (actual == null || actual.Siguiente == null)
-            {
-                Console.WriteLine("Posición no válida. No se ha eliminado ningún nodo.");
-                return;
-            }
-
-            actual.Siguiente = actual.Siguiente.Siguiente;
-        }
-
-        public double CalcularMediaGeometrica()
-        {
-            if (cabeza == null)
-                return 0;
-
-            double producto = 1;
-            int contador = 0;
-            Nodo actual = cabeza;
-
-            while (actual != null)
-            {
-                producto *= actual.Valor;
-                actual = actual.Siguiente;
-                contador++;
-            }
-
-            return Math.Pow(producto, 1.0 / contador);
-        }
-
         public double CalcularPromedio()
         {
             if (cabeza == null)
---
+++ ejercicio2-form/trabajo_en_equipo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2_form
+{
+    public class trabajo_en_equipo
+    {
+        /*
+
+        
+
+        public void EliminarEnPosicion(int posicion)
+        {
+            if (posicion <= 0 || cabeza == null)
+            {
+                Console.WriteLine("Posición no válida. No se ha eliminado ningún nodo.");
+                return;
+            }
+
+            if (posicion == 1)
+            {
+                cabeza = cabeza.Siguiente;
+                return;
+            }
+
+            Nodo actual = cabeza;
+            int contador = 1;
+
+            while (actual != null && contador < posicion - 1)
+            {
+                actual = actual.Siguiente;
+                contador++;
+            }
+
+            if (actual == null || actual.Siguiente == null)
+            {
+                Console.WriteLine("Posición no válida. No se ha eliminado ningún nodo.");
+                return;
+            }
+
+            actual.Siguiente = actual.Siguiente.Siguiente;
+        }
+
+        public double CalcularMediaGeometrica()
+        {
+            if (cabeza == null)
+                return 0;
+
+            double producto = 1;
+            int contador = 0;
+            Nodo actual = cabeza;
+
+            while (actual != null)
+            {
+                producto *= actual.Valor;
+                actual = actual.Siguiente;
+                contador++;
+            }
+
+            return Math.Pow(producto, 1.0 / contador);
+        }
+
+        */
+    }
+}
